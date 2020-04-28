ce0fb1e5cc5e1f70f7b93c4f6d87180fc52dbe11
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-04-28 17:17:15 +0000

    Give the PEP8 style

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,8 +7,10 @@ if number < 0:
 else:
     lastDigit = number % 10
 if lastDigit > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, lastDigit))
+    print("Last digit of {} is {} and is greater than 5".format
+          (number, lastDigit))
 elif lastDigit == 0:
     print("Last digit of {} is {} and is 0".format(number, lastDigit))
 elif lastDigit < 6 and lastDigit != 0:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, lastDigit))
+    print("Last digit of {} is {} and is less than 6 and not 0".format
+          (number, lastDigit))

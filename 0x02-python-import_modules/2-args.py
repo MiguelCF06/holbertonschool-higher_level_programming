fa5ce75ae32d2d76b0c1c66818e9cf5e94056362
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-04-30 17:10:05 +0000

    [Fixed]Spaces and declaration variables

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
 
-
     from sys import argv
 
     if (len(argv)-1 == 0):
@@ -10,8 +9,8 @@ if __name__ == "__main__":
         print("{} argument:".format(len(argv) - 1))
         print("{}: {}".format(len(argv)-1, argv[1]))
     else:
-        print("{} arguments:".format(len(argv) - 1))
         j = 1
+        print("{} arguments:".format(len(argv) - 1))
         while j <= len(argv)-1:
             print("{:d}: {}".format(j, argv[j]))
             j = j + 1

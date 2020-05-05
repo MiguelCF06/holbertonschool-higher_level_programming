5b90b8a73b7ba1735de6dda5fc65f1b9ce635034
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-05-05 18:11:25 +0000

    [Updated]Fixed conditional

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == None:
-        sentence[:1] = None
-    return len(sentence), sentence[:1]
+    return (len(sentence), sentence[0] if len(sentence) > 0 else None)

03704da0989df9783ff203adb58d25551a9a9495
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-05-05 17:47:14 +0000

    [Updated]Fixed PIP 8 Style

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 
-
 def print_reversed_list_integer(my_list=[]):
-
-    my_copy = my_list[:]
-    my_copy.reverse()
-    if my_copy:
-        for x in range(len(my_copy)):
-            print("{}".format(my_copy[x]))
+    if my_list:
+        for x in my_list[::-1]:
+            print("{}".format(x))

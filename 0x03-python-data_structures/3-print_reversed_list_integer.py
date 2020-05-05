75040278c9b9bdb53bd800b9881f5383f2dba66e
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-05-05 17:42:10 +0000

    [Updated]Changed while by a for

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    x = len(my_list)
-    while x > 0:
-        print("{}".format(my_list[x-1]))
-        x = x - 1
+    my_copy = my_list[:]
+    my_copy.reverse()
+    for x in range(len(my_copy)):
+        print("{}".format(my_copy[x]))

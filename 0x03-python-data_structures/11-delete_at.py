6304c0884912d8578a7593820d6181694a1dc559
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-05-05 18:14:02 +0000

    [Updated]Fixed PIP 8 Style

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     my_copy = my_list[:]
-    if idx < 0:
+    if idx < 0 or idx > len(my_list):
         return my_list
-    elif idx > len(my_list):
-        return my_list
-    elif idx > 0 and idx < len(my_list):
-        del my_copy[idx]
+    del my_copy[idx]
     return my_copy

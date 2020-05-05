c08d8bf799ef5378a10291eea8fc96f349d91a77
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-05-05 17:27:06 +0000

    [Updated]PIP 8 style

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    x = 0
-    while x < len(my_list):
+    for x in range(len(my_list)):
         print("{}".format(my_list[x]))
-        x = x + 1

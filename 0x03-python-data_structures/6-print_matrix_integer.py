e3b59cd63ad4db3de87e01db1c3fe75c52fc35b7
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-05-05 17:56:00 +0000

    [Updated]Removed nested fors

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
+
 def print_matrix_integer(matrix=[[]]):
-    for x in range(0, len(matrix)):
-        for y in range(0, len(matrix[x])):
-            if y == len(matrix[x]) - 1:
-                print("{}".format(matrix[x][y]), end="")
-            else:
-                print("{}".format(matrix[x][y]), end=" ")
-        print()
-            
+    for x in matrix:
+        print(" ".join("{:d}".format(i) for i in elm))

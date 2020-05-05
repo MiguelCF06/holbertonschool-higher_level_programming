9f870d5f1e004441c9b812075b951a8a4ddaabdd
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-05-05 17:53:44 +0000

    [Updated]Fixed condititonal

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     my_copy = my_list[:]
-    if idx < 0 or idx > len(my_list):
-        return my_copy
+    if idx < 0 or idx >= len(my_list):
+        return my_list
 
     my_copy[idx] = element
     return my_copy

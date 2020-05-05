b8cb663a4fb66568ef564be7ef36708c9e95b675
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-05-05 17:36:33 +0000

    [Updated]Removed the while

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,9 +5,5 @@ def replace_in_list(my_list, idx, element):
     elif idx > len(my_list):
         return my_list
     elif idx >= 0 and idx <= len(my_list):
-        x = 0
-        while my_list[x] <= idx + 1:
-            if my_list[x] == idx + 1:
-                my_list[x] = element
-                return my_list
-            x = x + 1
+        my_list[idx] = element
+        return my_list

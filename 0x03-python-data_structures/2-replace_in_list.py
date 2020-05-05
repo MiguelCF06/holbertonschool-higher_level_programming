47c122d2d5e7ae87b1bd0c725f7734e2591b2429
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-05-05 17:38:29 +0000

    [Updated]Fixed conditionals

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx < 0:
+    if idx < 0 or len(my_list) <= idx:
         return my_list
-    elif idx > len(my_list):
-        return my_list
-    elif idx >= 0 and idx <= len(my_list):
-        my_list[idx] = element
-        return my_list
+
+    my_list[idx] = element
+    return my_list

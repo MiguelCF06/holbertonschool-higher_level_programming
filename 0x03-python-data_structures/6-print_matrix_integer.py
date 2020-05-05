b83d1ea98586969525ed39774ca569d04db00686
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-05-05 17:57:13 +0000

    [Updated]Fixed syntax

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def print_matrix_integer(matrix=[[]]):
     for x in matrix:
-        print(" ".join("{:d}".format(i) for i in elm))
+        print(" ".join("{:d}".format(i) for i in x))

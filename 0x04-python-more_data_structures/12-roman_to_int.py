d2226f2b633749de3fa54dede5e16d38d245bc9b
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-05-07 17:54:11 +0000

    [Update]Fixed PIP 8 style

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,16 +6,16 @@ def roman_to_int(roman_string):
         while (x < len(roman_string)):
             simb1 = valueRoman(roman_string[x])
             if (x+1 < len(roman_string)):
-                simb2 = valueRoman(roman_string[x + 1])
+                simb2 = valueRoman(roman_string[x+1])
                 if (simb1 >= simb2):
                     res = res + simb1
-                    x = x + 1
+                    x = x+1
                 else:
                     res = res + simb2 - simb1
-                    x = x + 2
+                    x = x+2
             else:
                 res = res + simb1
-                x = x + 1
+                x = x+1
         return res
 
     return 0

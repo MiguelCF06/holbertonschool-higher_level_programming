1f293b7261e974b05c56384053d2b6549e121e66
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-05-07 17:45:24 +0000

    [Update]First Update

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if type(roman_string) == str:
+        res = 0
+        x = 0
+        while x < len(roman_string):
+            simb1 = valueR(roman_string[x])
+            if x + 1 < len(roman_string):
+                simb2 = valueR(roman_string[x+1])
+                if simb1 >= simb2:
+                    res = res + simb1
+                    x = x + 1
+                else:
+                    res = res + simb2 - simb1
+                    x = x + 2
+            else:
+                res = res + simb1
+                x = x + 1
+        return res
+    return 0
+
+def valueR(r):
+    if (r == 'I'):
+        return 1
+    if (r == 'V'):
+        return 5
+    if (r == 'X'):
+        return 10
+    if (r == 'L'):
+        return 50
+    if (r == 'C'):
+        return 100
+    if (r == 'D'):
+        return 500
+    if (r == 'M'):
+        return 1000
+    return -1

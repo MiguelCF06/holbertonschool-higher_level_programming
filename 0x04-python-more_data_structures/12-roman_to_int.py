b156d760e2260d140320d9d67a9346ac205126ce
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-05-07 18:16:13 +0000

    [Update]Fixed PIP 8 style

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python3
+def valueRoman(r):
+    if r == 'I':
+        return 1
+    if r == 'V':
+        return 5
+    if r == 'X':
+        return 10
+    if r == 'L':
+        return 50
+    if r == 'C':
+        return 100
+    if r == 'D':
+        return 500
+    if r == 'M':
+        return 1000
+    return -1
+
 def roman_to_int(roman_string):
     if type(roman_string) == str:
         res = 0
@@ -21,19 +38,3 @@ def roman_to_int(roman_string):
                 x += 1
         return res
     return 0
-def valueRoman(r):
-    if r == 'I':
-        return 1
-    if r == 'V':
-        return 5
-    if r == 'X':
-        return 10
-    if r == 'L':
-        return 50
-    if r == 'C':
-        return 100
-    if r == 'D':
-        return 500
-    if r == 'M':
-        return 1000
-    return -1

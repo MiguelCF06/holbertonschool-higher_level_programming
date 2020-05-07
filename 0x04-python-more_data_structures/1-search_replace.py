2391d5baefd92b0a34fa8eff70f2cf22929683d0
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-05-07 17:18:22 +0000

    [Updated]Fixed PIP 8 style

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list[:]
-    for i, x in enumerate(new_list): 
+    for i, x in enumerate(new_list):
         if x == search:
             new_list[i] = replace
     return new_list

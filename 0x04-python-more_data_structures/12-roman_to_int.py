94e57ab00ecc40c716e55bc52ff5d2a782553a89
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-05-07 17:59:12 +0000

    [Update]Fixed PIP 8 style

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,35 +3,34 @@ def roman_to_int(roman_string):
     if type(roman_string) == str:
         res = 0
         x = 0
-        while (x < len(roman_string)):
+        while x < len(roman_string):
             simb1 = valueRoman(roman_string[x])
-            if (x+1 < len(roman_string)):
-                simb2 = valueRoman(roman_string[x+1])
-                if (simb1 >= simb2):
+            if x + 1 < len(roman_string):
+                simb2 = valueRoman(roman_string[x + 1])
+                if simb1 >= simb2:
                     res = res + simb1
-                    x = x+1
+                    x = x + 1
                 else:
                     res = res + simb2 - simb1
-                    x = x+2
+                    x = x + 2
             else:
                 res = res + simb1
-                x = x+1
+                x = x + 1
         return res
-
     return 0
 def valueRoman(r):
-    if (r == 'I'):
+    if r == 'I':
         return 1
-    if (r == 'V'):
+    if r == 'V':
         return 5
-    if (r == 'X'):
+    if r == 'X':
         return 10
-    if (r == 'L'):
+    if r == 'L':
         return 50
-    if (r == 'C'):
+    if r == 'C':
         return 100
-    if (r == 'D'):
+    if r == 'D':
         return 500
-    if (r == 'M'):
+    if r == 'M':
         return 1000
     return -1

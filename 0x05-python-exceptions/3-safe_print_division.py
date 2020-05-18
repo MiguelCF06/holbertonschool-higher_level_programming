b32676bb6c526cf3afeb6ec55c4e459a3321694a
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-05-18 17:05:28 +0000

    [Updated]Fixed the finally: print

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,5 +5,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         res = None
     finally:
-        print("Inside Result: {}".format(res))
+        print("Inside result: {}".format(res))
     return res

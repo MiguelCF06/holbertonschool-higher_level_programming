c5d5d8957a9d3d31655223fed430228509ff6240
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-05-19 15:33:24 +0000

    [Update]First Update

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Defined a class square """
+
+class Square:
+    """ The square class 
+
+    Attributes:
+    __size : size of a side of the square
+    """
+    def __init__(self, size):
+        """ Initialize the square
+        Arguments:
+                 size : size of a side of the square
+
+        Return: None
+        """
+        self.__size = size

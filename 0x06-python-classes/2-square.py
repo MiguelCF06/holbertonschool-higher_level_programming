dd802ff522ff36b184f3d0b3aa65f060c66ad9b3
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-05-19 15:55:10 +0000

    [Updated]Fixed the order of the conditionals

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -13,8 +13,9 @@ class Square:
         Return : None
         """
         if type(size) is int:
-            self.__size = size
             if size < 0:
                 raise ValueError("Size must be >= 0")
+            else:
+                self.__size = size
         else:
             raise TypeError("Size must be an integer")

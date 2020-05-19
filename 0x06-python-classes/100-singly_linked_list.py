44e7c04fde6eb5ab9ac9d722ab3e4c9597c47640
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-05-19 21:44:25 +0000

    [Updated]Fixed docstrings of documentation

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,35 +1,42 @@
 #!/usr/bin/python3
+"""Write a Node class for a singly linked list"""
+
+
 class Node:
     """Represents a node in a singly linked list
+
     Attributes:
         __data (int): data stored inside the node
         __next_node (Node): next node in the linked list
     """
     def __init__(self, data, next_node=None):
         """Initializes the node
+
         Arguments:
             data (int): data stored inside the node
             next_node (Node): next node in the linked list
+
         Returns: None
         """
-        self.__data = data
-        self.__next_node = next_node
+        self.data = data
+        self.next_node = next_node
 
 
     @property
     def data(self):
         """getter of __data
-        Returns:
-            data stored inside the node
+
+        Returns: data stored inside the node
         """
         return self.__data
-
 
     @data.setter
     def data(self, value):
         """setter of __data
-        Arguments:
-            value (int): data stored insite the node
+
+        Arguments:            
+              value (int): data stored insite the node
+
         Returns: None
         """
         if type(value) is int:
@@ -40,8 +47,8 @@ class Node:
     @property
     def next_node(self):
         """getter of __next_node
-        Returns:
-           the next node in the linked list
+
+        Returns: the next node in the linked list
         """
         return self.__next_node
 
@@ -49,10 +56,11 @@ class Node:
     @next_node.setter
     def next_node(self, value):
         """setter of __next_node
+
         Args:
             value (Node): next node in the linked list
-        Returns:
-            None
+
+        Returns: None
         """
         if value is None or isinstance(value, Node):
             self.__next_node = value
@@ -61,17 +69,33 @@ class Node:
 
     def __str__(self):
         """String representation of Node instance
-        Returns:
-            Formatted string representing the node
+
+        Returns: Formatted string representing the node
         """
         return str(self.__data)
 
 class SinglyLinkedList:
+     """Represents a single linked list
+
+    Attributes:
+        __head (Node): head of the linked list
+    """
     def __init__(self):
+        """Initializes the linked list
+
+        Returns: None
+        """
         self.__head = None
 
 
     def sorted_insert(self, value):
+        """ inserts a new Node instance into the correct sorted position
+
+        Args:
+            value (int): data stored inside the new node
+
+        Returns: None
+        """
         new_node = Node(value)
         temporal = self.__head
 
@@ -88,6 +112,10 @@ class SinglyLinkedList:
         temporal.next_node = new_node
 
     def __str__(self):
+        """String representation of SinglyLinkedList instance
+
+        Returns: Formatted string representing the linked list
+        """
         string = ""
         temporal = self.__head
         while temporal is not None:

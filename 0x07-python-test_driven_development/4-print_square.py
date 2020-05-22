d462e9e0a3029537bc3ca24b74abf56d2d90bf27
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-05-22 17:20:37 +0000

    [Updated]Added a conditional for booleans

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -10,11 +10,14 @@ def print_square(size):
     An argument size type integer
     representing the size of the square
     """
-    if not isinstance(size, int):
+    if isinstance(size, bool):
+        raise TypeError("size must be an integer")
+    elif not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    for rows in range(size):
-        for cols in range(size):
-            print("#", end="")
-        print()
+    if isinstance(size, int):
+        for rows in range(size):
+            for cols in range(size):
+                print("#", end="")
+            print()

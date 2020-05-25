be2d5c0476b60709dbcca839491b5b88eb2cbb59
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-05-25 15:24:45 +0000

    [Updated]Changed the and by or in the perimeter conditional

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -44,7 +44,7 @@ class Rectangle:
 
     def perimeter(self):
         """ Rectangle perimeter """
-        if self.__width == 0 and self.__height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         else:
             return (self.__width * 2) + (self.__height * 2)

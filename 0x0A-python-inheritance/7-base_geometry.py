e87de7887e1946e9abb402e6969fcfbe0b793e35
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-06-02 23:08:37 +0000

    [Updated]Added a string conversion format

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -10,6 +10,6 @@ class BaseGeometry():
     def integer_validator(self, name, value):
         """ Validate the base geometry """
         if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError("{:s} must be greater than 0".format(name))

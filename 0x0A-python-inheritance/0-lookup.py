149b4481007a4350c1934dc47ed97560eed5a83f
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-06-02 14:29:40 +0000

    [Update]First Update

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Defined a derived class """
+
+def lookup(obj):
+    """Return a list with the available
+    attributes and methods of the object
+    """
+    return dir(obj)

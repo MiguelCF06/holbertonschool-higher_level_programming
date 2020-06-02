358bf84ac9b85fc9cc76fb04de6eaf10ca932ad6
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-06-02 23:06:02 +0000

    [Updated]Removed a conditional

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -9,8 +9,7 @@ class BaseGeometry():
 
     def integer_validator(self, name, value):
         """ Validate the base geometry """
-        if isinstance(name, str):
-            if type(value) is not int:
-                raise TypeError("{} must be an integer".format(name))
-            if value <= 0:
-                raise ValueError("{} must be greater than 0".format(name))
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))

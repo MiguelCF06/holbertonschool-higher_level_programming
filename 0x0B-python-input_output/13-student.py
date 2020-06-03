f0ec2396cd893a42ecc60547ffb19368cf174af6
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-06-03 18:01:33 +0000

    [Update]First Update

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Class Student content
+"""
+
+class Student:
+    """ A student class """
+    def __init__(self, first_name, last_name, age):
+        """ initialize the class """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Returns a dictionary representation of the class """
+        if attrs is None:
+            return self.__dict__
+        myDict = {}
+        for atribute in attrs:
+            try:
+                myDict[atribute] = self.__dict__[atribute]
+            except:
+                pass
+        return myDict
+
+    def reload_from_json(self, json):
+        """ Replaces all attributes of the student instance """
+        for key in json:
+            try:
+                setattr(self, key, json[key])
+            except:
+                pass

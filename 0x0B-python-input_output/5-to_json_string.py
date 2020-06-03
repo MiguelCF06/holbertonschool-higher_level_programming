1203afc18e4928fe5d6bd26ae0713752bcf60c3e
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-06-03 16:09:23 +0000

    [Update]First Update

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import json
+"""
+Return a JSON representation of an object
+"""
+
+def to_json_string(my_obj):
+    """ String JSON representation of the object """
+    return json.dumps(my_obj)

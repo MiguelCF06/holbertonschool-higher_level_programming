1215bd3121829cddae75685b289c5899669741e6
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-06-03 15:54:02 +0000

    [Update]First Update

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Reads n lines of a text file
+"""
+
+def read_lines(filename="", nb_lines=0):
+    """ Open the file and print n lines """
+    with open(filename, "r") as f:
+        if nb_lines <= 0:
+            for lines in f:
+                print(lines, end="")
+        for i in range(nb_lines):
+            lines = f.readline()
+            print(lines, end="")

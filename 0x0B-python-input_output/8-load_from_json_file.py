104e113e1c61ad12bde8623684494899636a294b
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-06-03 22:03:19 +0000

    [Add]Added the docstring to the function

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -2,9 +2,12 @@
 """
 Creates an object from a JSON file
 """
+
+
 import json
 
 def load_from_json_file(filename):
+    """creates an Object from a "JSON file" """
     with open(filename, "r") as f:
         obj = json.load(f)
         return obj

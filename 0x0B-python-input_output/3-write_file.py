9371dc21c2b6a7baf4f6e25cc0e02197c7e8dcce
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-06-03 16:00:00 +0000

    [Update]First Update

---
+++ 0x0B-python-input_output/3-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Writes a string to a txt file
+"""
+
+def write_file(filename="", text=""):
+    """ Open a file in write mode """
+    with open(filename, "w") as f:
+        numOfChar = f.write(text)
+        return numOfChar

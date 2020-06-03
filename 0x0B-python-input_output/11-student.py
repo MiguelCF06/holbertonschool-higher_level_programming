b949f364a257d80662d8a9d5eb72e19c55a3620e
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-06-03 17:40:24 +0000

    [Update]First Update

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Class Student content
+"""
+
+class Student:
+    """ A student class """
+    def __init__(self, first_name, last_name, age):
+        """ initialize the class """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ Returns a dictionary representation of the class """
+        return self.__dict__

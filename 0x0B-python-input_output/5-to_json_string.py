667dd912eace981430947a2136b12d7f2cccae91
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-06-03 21:56:44 +0000

    [Fixed]Change of line of the import

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import json
 """
 Return a JSON representation of an object
 """
+import json
 
 def to_json_string(my_obj):
     """ String JSON representation of the object """

b950d331ce895e0c1c5693bcc3eac9aef1db5825
Miguel Angel Cipamocha Figueredo <60364713+MiguelCF06@users.noreply.github.com>
2020-06-03 17:05:30 -0500

    [Updated]Added a blank line

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -3,6 +3,7 @@
 Returns a dictionary description with simple data structure
 """
 
+
 def class_to_json(obj):
     """ Makes the dictionary for JSON serialization of an obj """
     return obj.__dict__

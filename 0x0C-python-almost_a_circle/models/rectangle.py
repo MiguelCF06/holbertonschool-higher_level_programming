e44b99c821fdba5802bb7e1c8680c4eec263a6e4
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-06-07 18:20:34 +0000

    [Fixed]Some docstrings and lines

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -25,7 +25,7 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """ Setter width """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -33,13 +33,13 @@ class Rectangle(Base):
 
     @property
     def height(self):
-        """ Getter height """
+        """Getter height"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """ Setter height """
-        if not isinstance(value, int):
+        """Setter height"""
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
@@ -47,13 +47,13 @@ class Rectangle(Base):
 
     @property
     def x(self):
-        """ Getter x """
+        """Getter x"""
         return self.__x
 
     @x.setter
     def x(self, value):
-        """ Setter x """
-        if not isinstance(value, int):
+        """Setter x"""
+        if type(value) is not int:
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
@@ -61,25 +61,25 @@ class Rectangle(Base):
 
     @property
     def y(self):
-        """ Getter y """
+        """Getter y"""
         return self.__y
 
     @y.setter
     def y(self, value):
-        """ Setter y """
-        if not isinstance(value, int):
+        """Setter y"""
+        if type(value) is not int:
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
 
     def area(self):
-        """ Method that returns the area of the rectangle """
+        """Method that returns the area of the rectangle"""
         return self.__width * self.__height
 
     def display(self):
-        """ Method that prints in stdout the rectangle instance with
-        char # """
+        """Method that prints in stdout the rectangle instance with
+        char #"""
         rep = "#"
         for space in range(self.__y):
             print()
@@ -91,12 +91,13 @@ class Rectangle(Base):
             print()
 
     def __str__(self):
-        """ String representation """
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y,
-                                                self.__width, self.__height)
+        """String representation"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x,
+                                                       self.__y, self.__width,
+                                                       self.__height)
 
     def update(self, *args, **kwargs):
-        """ Assigns an argument to each attribute """
+        """Assigns an argument to each attribute"""
         if len(args):
             try:
                 self.id = args[0]
@@ -119,8 +120,8 @@ class Rectangle(Base):
                 self.y = kwargs["y"]
 
     def to_dictionary(self):
-        """ Returns a dictionary representation
-        of a Rectangle """
+        """Returns a dictionary representation
+        of a Rectangle"""
         myDict = {}
         myDict["id"] = self.id
         myDict["width"] = self.width

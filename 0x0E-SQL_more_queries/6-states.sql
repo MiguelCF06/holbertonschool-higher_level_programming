9c767a909a9a34d8619eb5d2aec0e1aff27ebb79
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-07-14 16:24:34 +0000

    [Update]First Update

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- Creates the database hbtn_0d_usa and the table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
+(id INT AUTO_INCREMENT NOT NULL PRIMARY KEY UNIQUE,
+name VARCHAR(256) NOT NULL);

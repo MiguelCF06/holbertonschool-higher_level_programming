4d3b00f49d559bd4a9ec1e0fb843a7a4d4619bef
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-07-14 15:24:34 +0000

    [Update]First Update

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Creates a user 'user_0d_1'
+-- The user should have all privileges on your MySQL server
+-- The user_0d_1 password should be set to user_0d_1_pwd
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost
+IDENTIFIED BY 'user_0d_1_pwd';

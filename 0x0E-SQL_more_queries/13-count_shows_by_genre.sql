600fc29d7ad532cb70d7ed6fb60f435f575e2767
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-07-15 00:04:12 +0000

    [Update]First Update

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -5,6 +5,6 @@
 -- First column must be called genre
 -- Second column must be called number_of_shows
 -- Dont display a genre that doesnt have any shows linked
-SELECT tv_genres.name AS genres, COUNT(*) AS n_linshows FROM tv_genres
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_show_genres.genre_id ORDER BY n_linshows DESC;
+GROUP BY tv_show_genres.genre_id ORDER BY number_of_shows DESC;

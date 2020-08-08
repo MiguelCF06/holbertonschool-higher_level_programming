fab6882cac86401a00875777445407ee5446f923
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-08-08 16:37:19 +0000

    [Updated]Fixed

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -16,6 +16,7 @@ if __name__ == "__main__":
                 (sys.argv[4],))
     rows = cur.fetchall()
     for row in rows:
-        print(row)
+        if row[1] == sys.argv[4]:
+            print(row)
     cur.close()
     db.close()

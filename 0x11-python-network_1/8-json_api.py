83bce55f1c6f824a4caa9c267315e0740ebbf947
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-08-24 18:17:57 +0000

    [Update]First Update

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,7 +8,7 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    url = "http://19f003bb820b.22216bed.hbtn-cod.io:5000/search_user"
+    url = "http://0.0.0.0:5000/search_user"
     if len(sys.argv) == 2:
         q = sys.argv[1]
     else:

97e4155de22137f171d50883ad22279ff7dd650e
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-09-15 16:34:35 +0000

    [Update]First Update

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+let x;
+
+for (x = 0; x < 3; x++) {
+  if (x === 0) {
+    console.log('C is fun');
+  } else if (x === 1) {
+    console.log('Python is cool');
+  } else {
+    console.log('Javascript is amazing');
+  }
+}

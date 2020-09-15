c21f6a9e9f10f1423a914e39e0edc9c92e595f81
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-09-15 17:33:47 +0000

    [Update]First Update

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+let i;
+const square = [];
+const arg = Number(process.argv[2]);
+
+if (!arg) {
+  console.log('Missing size');
+} else {
+  for (i = 0; i < arg; i++) {
+    console.log('X'.repeat(arg));
+  }
+}

c0ba142d52d1b39e3dd9f7ed825102fa22f43f2d
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-09-22 17:55:06 +0000

    [Updated]Handled error in writeFile

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,10 +3,14 @@ const request = require('request');
 const fs = require('fs');
 const url = process.argv[2];
 const fileToStore = process.argv[3];
-request(url, { json: true }, (err, res, body) => {
+request(url, function (err, res, body) {
   if (err) {
     console.log(err);
   } else {
-    fs.writeFile(fileToStore, body, 'utf-8');
+    fs.writeFile(fileToStore, body, 'utf-8', function (err) {
+      if (err) {
+        console.log(err);
+      }
+    });
   }
 });

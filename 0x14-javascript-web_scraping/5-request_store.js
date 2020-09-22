519bf3f41513b5993a56207da2fa40f73fcdd88d
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-09-22 14:13:46 +0000

    [Update]First Update

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const fileToStore = process.argv[3];
+request(url, { json: true }, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(fileToStore, body, function (err) {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
